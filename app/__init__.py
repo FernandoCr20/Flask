16dd600a42f09ed942cd84f6dbcc1becb9e0f5e7
FERNANDO CHAVES RODRIGUES <202308423991@alunos.estacio.br>
2024-04-19 10:03:30 -0300

    CRUD sqlAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+
+db = SQLAlchemy(app)
+
+''' @app.route("/")
+def index():
+    # return "<h1> Minha aplicacao em flask </h1>"
+    return render_template("index.html") '''
