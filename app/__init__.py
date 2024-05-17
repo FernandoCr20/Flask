56cd6f828fc2ad48d6fc6bc4bd79c80344177fae
FERNANDO CHAVES RODRIGUES <202308423991@alunos.estacio.br>
2024-05-17 09:08:22 -0300

    Finalizando API

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 
